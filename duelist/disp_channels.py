1c4ae85657adaf8c11fc7298fc44de77b95e6c92
Hansimov <591172499@qq.com>
2023-08-03 17:27:16 +0800

    [Refactor] Rename utils.py

--- duelist/disp_channels.py
+++ duelist/disp_channels.py
@@ -1,8 +1,10 @@
+from pathlib import Path
 import os
 import sys
 import mido
-sys.path.append('..')
-from _utils import *
+
+sys.path.append(Path(__file__).parents[1])
+from utils import *
 
 """
 [Definitions]
@@ -27,7 +29,7 @@ from _utils import *
 
 """
 
-fn = 'passionate-duelist.mid'
+fn = "passionate-duelist.mid"
 
 md = mido.MidiFile(fn)
 
@@ -35,16 +37,18 @@ tracks = md.tracks
 
 print(len(tracks), md.length, md.ticks_per_beat)
 
+
 def disp_track(trk):
-    with open('tracks.log', 'w') as wf:
-            # trk = sorted(trk, key=lambda msg: msg.time)
-            # print(trk, file=wf)
-            print(len(trk), file=wf)
-            for msg in trk:
-                print(f'\t{msg.type} {msg}', file=wf)
+    with open("tracks.log", "w") as wf:
+        # trk = sorted(trk, key=lambda msg: msg.time)
+        # print(trk, file=wf)
+        print(len(trk), file=wf)
+        for msg in trk:
+            print(f"\t{msg.type} {msg}", file=wf)
+
 
 def disp_program(msg):
-    if msg.type == 'program_change':
+    if msg.type == "program_change":
         prog = msg.program
         chan = msg.channel
         if chan == 9:
@@ -53,5 +57,6 @@ def disp_program(msg):
             inst = patch_num_table[prog]
 
         # print(f'channel: {chan:<3} program: {prog:<4} instrument: {inst}')
+
 
 disp_track(tracks[0])
