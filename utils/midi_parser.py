2307686e0fab0b6ec84fd4ab9750e973b35a741a
Hansimov <591172499@qq.com>
2023-08-04 16:40:34 +0800

    [Doc] More tips for everything about time

--- utils/midi_parser.py
+++ utils/midi_parser.py
@@ -1,80 +1,61 @@
 import mido
 
-"""
-[Definitions]
-    BPM: beats per minute
-    TPB: ticks per beat
-    tempo: μs per beat
-
-[Default values]
-    1 beat = 1 quarter-note = 6 MIDI clocks
-
-    tempo = 500000 
-    -> 500000 μs (0.5s) per beat
-    -> 1 minute per 120 beats
-    -> BPM = 120
-
-    ticks per beat = ?
-
-[Example]
-    ticks_per_beat = 480
-    tempo = μs_per_beat = 800000
-    -> 1 tick = μs_per_beat / ticks_per_beat ≈ 1666.67 μs
-
-"""
-
-
-""" [The structure of MidiFile in mido]
+""" [Structure of MidiFile in mido]
+- Structure:
+    - MidiFile > MidiTracks > Messages 
 
 * Standard MIDI Files — Mido 1.3.0 documentation
   * https://mido.readthedocs.io/en/stable/files/midi.html
 
-MidiFile > MidiTracks > Messages
+* Message Types — Mido 1.3.0 documentation
+  * https://mido.readthedocs.io/en/stable/message_types.html
+
+* Meta Message Types — Mido 1.3.0 documentation
+  * https://mido.readthedocs.io/en/stable/meta_message_types.html?highlight=key_signature
 
 ## Example args with values:
 
-MidiFile:
-  - type: 1
-    - ticks_per_beat = 480
-    - tracks = [...Messages...]
-Message:
-  - type: (meta) 'time_signature'
-    - numerator = 4
-    - denominator = 4
-    - clocks_per_click = 24
-    - notated_32nd_notes_per_beat = 8
-    - time = 0
-  - type: (meta) 'key_signature'
-    - key = 'B'
-    - time = 0
-  - type: 'control_change'
-    - channel = 0
-    - control = 121
-    - value = 0
-    - time = 0
-  - type: 'program_change'
-    - channel = 0
-    - program = 40
-    - time = 0
-  - type: (meta) 'midi_port'
-    - port = 0
-    - time = 0
-  - type: 'set_tempo'
-    - tempo = 419581
-    - time = 1890
-  - type: 'note_on'
-    - channel = 0
-    - note = 83
-    - velocity = 85
-    - time = 14670
-  - type: (meta) 'end_of_track'
-    - time = 1
+- MidiFile:
+    - type: 1
+        - ticks_per_beat = 480
+        - tracks = [...Messages...]
+- Message:
+    - type: (meta) 'time_signature'
+        - numerator = 4
+        - denominator = 4
+        - clocks_per_click = 24
+        - notated_32nd_notes_per_beat = 8
+        - time = 0
+    - type: (meta) 'key_signature'
+        - key = 'B'
+        - time = 0
+    - type: 'control_change'
+        - channel = 0
+        - control = 121
+        - value = 0
+        - time = 0
+    - type: 'program_change'
+        - channel = 0
+        - program = 40
+        - time = 0
+    - type: (meta) 'midi_port'
+        - port = 0
+        - time = 0
+    - type: 'set_tempo'
+        - tempo = 419581
+        - time = 1890
+    - type: 'note_on'
+        - channel = 0
+        - note = 83
+        - velocity = 85
+        - time = 14670
+    - type: (meta) 'end_of_track'
+        - time = 1
 
 
 ## Example console output:
 
 `mido.MidiFile(midi_filepath)`:
-
 MidiFile(type=1, ticks_per_beat=480, tracks=[
   MidiTrack([
     MetaMessage('time_signature', numerator=4, denominator=4, clocks_per_click=24, notated_32nd_notes_per_beat=8, time=0),
@@ -110,20 +91,58 @@ MidiFile(type=1, ticks_per_beat=480, tracks=[
   ])
 ])
 
-
-## Units and terms
-
-- time
-  - Delta time in ticks. Must be an integer.
-  - See more:
-    - https://mido.readthedocs.io/en/stable/files/midi.html#about-the-time-attribute
-
 """
 
-""" [Structure of Converted Dataframe of Notes]
-columns:
-    note, channel, velocity, time_in_seconds, instrument
 
+""" Tips
+
+## Turn off Notes
+
+In many cases, a note is turned off not by a `note_off` message,
+but by a `note_on` message with `velocity` 0:
+
+```
+    Message('note_on', channel=7, note=90, velocity=92, time=1),
+    Message('note_on', channel=7, note=90, velocity=0, time=479),
+```
+ 
+"""
+
+""" Ticks, Beats, Tempo, Quarter-note, and everything about Time
+
+[Definitions]
+    - BPM
+        - beats per minute
+    - TPB
+        - ticks per beat
+    - tempo
+        - μs per beat
+    - `time` of Message in mido
+        - Delta time in ticks. Must be an integer.
+        - See more:
+        - https://mido.readthedocs.io/en/stable/files/midi.html#about-the-time-attribute
+
+[Default values]
+    1 beat = 1 quarter-note = 6 MIDI clocks
+
+    tempo = 500000 
+    -> 500000 μs (0.5s) per beat
+    -> 1 minute per 120 beats
+    -> BPM = 120
+
+    ticks per beat = ?
+
+[Example]
+    ticks_per_beat = 480
+    tempo = μs_per_beat = 800000
+    -> 1 tick = μs_per_beat / ticks_per_beat ≈ 1666.67 μs
+"""
+
+""" [Data Structure of Converted Dataframe of Notes]
+columns:
+    note, channel, instrument, velocity, start_time, end_time, duration, key
+metadata:
+    BPM
 """
 
 
